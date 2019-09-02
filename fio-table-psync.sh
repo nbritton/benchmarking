ac1af7859e7f8cd35c6f79199ae6c59e7c6fc297
Nikolas Britton <nbritton@users.noreply.github.com>
2019-09-01 20:13:27 -0500

    Create fio-table-psync.sh

---
+++ fio-table-psync.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+for direct in 0 1; do
+        for bs in 4k 8k 16k 32k 64k 128k 256k 512k 1024k; do
+                RESULTS=$(
+                        fio \
+                                --randrepeat=1 \
+                                --ioengine=psync \
+                                --direct=${direct} \
+                                --gtod_reduce=1 \
+                                --name=test \
+                                --filename=test \
+                                --iodepth=64 \
+                                --size=4G \
+                                --readwrite=randrw \
+                                --rwmixread=60 \
+                                --loops=10 \
+                                --bs=${bs} \
+                                --output-format=json;
+                );
+                printf "${direct}, ";
+                printf "${bs}, ";
+                echo $RESULTS | jq '.jobs[] | .read.bw_mean' | tr -d '\n';
+                printf ", ";
+                echo $RESULTS | jq '.jobs[] | .read.iops_mean' | tr -d '\n';
+                printf ", ";
+                echo $RESULTS | jq '.jobs[] | .write.bw_mean' | tr -d '\n';
+                printf ", ";
+                echo $RESULTS | jq '.jobs[] | .write.iops_mean' | tr -d '\n';
+                echo;
+                sleep 20;
+        done;
+done;
